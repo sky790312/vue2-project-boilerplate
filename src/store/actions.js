359128b00c8aefda5c5a63ec2d33702ffffffb52
Hu Kevin <sky790312yks@gmail.com>
2016-12-06 18:53:26 +0800

    change api to async await, use aciton to call api

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,9 +1,18 @@
+import api from '../api'
+
 export const increment = ({ commit }) => {
   commit('INCREMENT')
 }
 
 export const decrement = ({ commit }) => {
   commit('DECREMENT')
+}
+
+export const exampleCallApi = async ({ commit }, payload) => {
+  const urlEnd = '/example/apiurl'
+  const type = 'post'
+  const resp = await api.asyncFunction(urlEnd, type, payload)
+  return resp
 }
 
 /*
--- src/store/api.js
+++ src/store/api.js
@@ -2,17 +2,20 @@ import Vue from 'vue'
 
 const api = {
   serverURL: 'http://exampleServerUrl',
-  version: '/example/v1'
+  version: '/api/v1',
+  'timeout': { timeout: 20000 }
 }
 
-api.example = (exampleProperty) => {
-  return new Promise((resolve, reject) => {
-    const EXAMPLE_API_URL = api.serverURL + api.version + '/exampleApi?property=' + exampleProperty
-    Vue.http.get(EXAMPLE_API_URL, {}).then(resp => {
-      resolve(resp)
-    }, err => {
-      console.log('call example api error: ', err)
-      reject(err)
-    })
-  })
+// api.example = async (exampleProperty) => {
+//   const EXAMPLE_API_URL = api.serverURL + api.version + '/exampleApi?property=' + exampleProperty
+//   const resp = await Vue.http.get(EXAMPLE_API_URL, {})
+//   return resp
+// }
+
+api.asyncFunction = async (urlEnd, type, payload = {}) => {
+  let resp = {}
+  if (type === 'get') resp = await Vue.http.get(api.serverUrl + urlEnd, payload, api.timeout)
+  else if (type === 'post') resp = await Vue.http.post(api.serverUrl + urlEnd, payload, api.timeout)
+  else if (type === 'put') resp = await Vue.http.put(api.serverUrl + urlEnd, payload, api.timeout)
+  return resp
 }
