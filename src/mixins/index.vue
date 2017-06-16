b2d351678525ed30ed4f8205ec42492f75eb3c5c
kevin.hu <kevin.hu@kevinhude-iMac.local>
2017-06-16 15:27:53 +0800

    ad  unit test example - data, method, mixins data, mixins method, props data

---
+++ src/mixins/index.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  data () {
+    return {
+      mixinData: 'is mixins data'
+    }
+  },
+  methods: {
+    testMixin () {
+      // console.log('in testMixin')
+      return true
+    }
+  }
+}
+</script>
--- test/unit/specs/Hello.spec.js
+++ test/unit/specs/Hello.spec.js
@@ -1,13 +1,30 @@
 import Vue from 'vue'
-import Hello from 'src/components/Hello'
+import Hello from '@/components/Hello'
+
+const Vm = Vue.extend(Hello)
+const hello = new Vm().$mount()
 
 describe('Hello.vue', () => {
-  it('should render correct contents', () => {
-    const vm = new Vue({
-      el: document.createElement('div'),
-      render: (h) => h(Hello)
-    })
-    expect(vm.$el.querySelector('.hello h1').textContent)
-      .to.equal('Welcome to Your Vue.js App')
+  console.log('testing..')
+  console.log('hello component data: ', hello.msg)
+  console.log('hello component method: ', hello.test())
+  console.log('hello component mixins method: ', hello.testMixin())
+  console.log('hello component mixins data: ', hello.mixinData)
+  console.log('hello component props data: ', hello.title)
+
+  it('should render correct component data', () => {
+    expect(hello.msg).to.equal('Welcome to Your Vue.js App')
+  })
+  it('should render correct component method', () => {
+    expect(hello.test()).to.equal(1)
+  })
+  it('should render correct mixins method', () => {
+    expect(hello.testMixin()).to.equal(true)
+  })
+  it('should render correct mixins data', () => {
+    expect(hello.mixinData).to.equal('is mixins data')
+  })
+  it('should render correct component data', () => {
+    expect(hello.title).to.equal('is title')
   })
 })
