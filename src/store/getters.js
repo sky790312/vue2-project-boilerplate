b644ab00807debbfcc54ce4b7355447c7ecc2ad9
KevinHu <sky790312yks@gmail.com>
2017-09-26 00:39:03 +0800

    update example

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,1 +1,2 @@
 // export const count = state => state.count
+export const test = () => {}
--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,12 +1,12 @@
-// import api from './api'
+import api from '../api'
 
 // export const INCREMENT = state => state.count++
 //
 // export const DECREMENT = state => state.count--
 
-// export const CALLAPI = ({ dispatch, state }, exampleProperty, callback) => {
-//   api.example(exampleProperty).then(result => {
-//     console.log(result)
-//     callback()
-//   })
-// }
+export const CALLAPI = ({ dispatch, state }, exampleProperty, callback) => {
+  api.example(exampleProperty).then(result => {
+    console.log(result)
+    callback()
+  })
+}
